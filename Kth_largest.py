da479feb8026e46533ff6e2dcc90b76d81a8efd9
vscalar <vscalar02@gmail.com>
2024-09-30 12:38:04 +0900

    put randomness in partition for Kth_largest.py

--- Kth_largest.py
+++ Kth_largest.py
@@ -1,6 +1,7 @@
 class Solution:
     import sys
     sys.setrecursionlimit(10**6)
+    import random
     def findKthLargest(self, nums: list[int], k: int) -> int:
         def Merge(left: tuple, right: tuple) -> list[int]:
             merged = []
@@ -24,50 +25,50 @@ class Solution:
             
             left = MergeSort(tuple(nums[0:n//2]))
             right = MergeSort(tuple(nums[n//2:n]))
-
             return Merge(left, right)
         
         #get random value between left and right
         def GetPivot(left: int, right: int) -> int:
-            import random
-            return random.randint(left, right)
+            return self.random.randint(left, right)
             
-        #modify nums to [bigger]+[pivot]+[smaller], return a index where pivot exists
+        #modify nums to [bigger]+[pivot]+[smaller], return a index where pivot exists 
         def Partition(left: int, right: int, pivot: int) -> tuple:
             pivotVal = nums[pivot]
             nums[pivot], nums[right] = nums[right], nums[pivot]
-            store_index = left
-            occurence = 0
-            
+            store_index = left            
             for i in range(left, right):
-                if nums[i] >= pivotVal:  
-                    if nums[i] == pivotVal: occurence += 1
+                if nums[i] > pivotVal:  
                     nums[store_index], nums[i] = nums[i], nums[store_index]
                     store_index += 1
+                elif nums[i] == pivotVal:
+                    isRight = self.random.randint(0, 1)
+                    if isRight:
+                        nums[store_index], nums[i] = nums[i], nums[store_index]
+                        store_index += 1
             
             nums[right], nums[store_index] = nums[store_index], nums[right]
-            return store_index, occurence
-            
+            return store_index
+        
+        #search for k in nums[left:right+1]
         def GetKth(left: int, right: int, k: int) -> int:
             if left == right:
                 return nums[left]
             
-            if len(nums) <= 50:
-                sortedNums = MergeSort(nums)
-                return sortedNums[k]
+            #MergeSort if length is below 50
+            if right - left <= 50:
+                sortedNums = MergeSort(nums[left:right+1])
+                return sortedNums[k-1-left]
             
             pivotIndex = GetPivot(left, right)
-            pivotIndex, occurence = Partition(left, right, pivotIndex)
+            pivotIndex = Partition(left, right, pivotIndex)
 
-            
-            if k == pivotIndex:
+            if k - 1 == pivotIndex:
                 return nums[k]
             #search for bigger part
-            elif k < pivotIndex: 
-                if occurence > pivotIndex - k + 1: return nums[k]
+            elif k - 1 < pivotIndex: 
                 return GetKth(left, pivotIndex - 1, k)
             #search for smaller part
             else:
                 return GetKth(pivotIndex + 1, right, k) 
     
-        return GetKth(0, len(nums) - 1, k-1)
+        return GetKth(0, len(nums) - 1, k)
